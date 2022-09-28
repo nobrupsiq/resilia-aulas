9468c0b117a841dda768e9019b1b6f39eace02e5
bruno <bpires.siqueira@gmail.com>
2022-09-28 09:59:50 -0300

    exemplo1

--- modulo-1/aula-11-tech/script.js
+++ modulo-1/aula-11-tech/script.js
@@ -1,7 +1,34 @@
-var comeuTudo = 'sim';
+// var comeuTudo = 'sim';
 
-if (comeuTudo == 'sim') {
-    console.log('Sobremesa liberada!')
-} else {
-    console.log("Nada de  sobremesa")
-}
+// if (comeuTudo == 'sim') {
+//     console.log('Sobremesa liberada!')
+// } else {
+//     console.log("Nada de  sobremesa")
+// }
+
+
+// EXERCICIO DA AULA 11
+
+// var temperatura = 38;
+
+// if (temperatura >= 38)
+//     console.log('Você está com febre')
+// else
+//     console.log('nada de febre')
+
+
+// EXPLICIAÇÃO DA AULA
+
+// var nome = prompt('Digite o seu nome: ')
+// document.write("Bom dia, " + nome + '!<br>')
+// document.write('Tchau!')
+// window.alert('Nosso script de javascript na tela!')
+
+var n1 = 10;
+var n2 = 30;
+
+var soma = 0;
+
+soma = n1 + n2;
+
+alert(`O valor do calculo é ${soma}!`)
