bf4d041f3533eceb881b9b6c55503e1eb7abac4a
bruno <bpires.siqueira@gmail.com>
2022-09-28 11:17:34 -0300

    exercico proposto pelo professor

---
+++ modulo-1/aula-11-tech/codigo2.js
@@ -0,0 +1,15 @@
+
+var n1 = 4.5;
+var n2 = 8;
+var n3 = 7;
+var n4 = 9;
+
+var media = ((n1 + n2) + (n3 + n4)) / 4;
+
+if (media >= 7) {
+    alert(`A média é ${media} O aluno está APROVADO!`)
+} else if (media >= 5 && media < 7) {
+    alert(`A média é ${media} O aluno em RECUPERAÇÃO`)
+} else {
+    alert(`Média ${media} aluno REPROVADO`)
+}
