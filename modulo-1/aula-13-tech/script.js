324ea0d295306404e5045f431f496cf5f812e1e0
bruno <bpires.siqueira@gmail.com>
2022-09-30 09:24:20 -0300

    exercicio pratido

--- modulo-1/aula-13-tech/script.js
+++ modulo-1/aula-13-tech/script.js
@@ -10,9 +10,18 @@
 
 // var saborPizza = prompt('Digite o saber da pizza: ')
 
+// function criaTextoPreco(item, preco) {
+//     return `O item ${item} custa R$ ${preco}`
+// }
 
-function criaTextoPreco(item, preco) {
-    return `O item ${item} custa R$ ${preco}`
+// console.log(criaTextoPreco('Tomate', 0.98));
+
+var idade = prompt("Digite a idade:");
+
+if ((idade > 15 && idade < 18) || idade > 70) {
+  alert("O voto é opcional");
+} else if (idade < 16) {
+  alert("Não poderá votar");
+} else {
+  alert("Você pode votar!");
 }
-
-console.log(criaTextoPreco('Tomate', 0.98));
