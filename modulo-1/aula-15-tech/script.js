cffd0bc0b53fb20f7cfc5021625049b7fefda672
Bruno <bpires.siqueira@gmail.com>
2022-10-03 18:54:28 -0300

    aula 15 exercicio

---
+++ modulo-1/aula-15-tech/script.js
@@ -0,0 +1,38 @@
+// Atividade: Olha o loop
+
+/* ● Crie uma função chamada listaDecrescente. Sua função deve:
+    ○ Receber um número positivo
+    ○ Retornar uma string de números, em ordem decrescente, que vai do argumento
+    fornecido a zero
+    ○ Utilize “\n” na separação dos números para pular linhas
+
+
+function listaDecrescente(numero) {
+    var i = 0;
+    while (numero > i) {
+        console.log(numero);
+        numero--;
+    }
+}
+listaDecrescente(10);
+
+
+
+ ● Crie uma função chamada fizz. Sua função deve:
+    ○ Receber um número positivo
+    ○ Retornar uma string de números, em ordem crescente, na qual os números que são
+ múltiplos de 3 estão substituídos por fizz
+
+ ● Teste tanto a função listaDecrescente quanto fizz, com diferentes argumentos
+ */
+
+// function fizz(numero) {
+//     var n = 0;
+//     while (n < numero) {
+//         if (n % 3 == 0) {
+//             console.log(n);
+//         }
+//         n++;
+//     }
+// }
+// fizz(20);
