9f5f10d38664b9f8be3c96ae9df0fd08fc8677a5
Bruno <bpires.siqueira@gmail.com>
2022-10-03 22:54:14 -0300

    aula 16 imcompleta

---
+++ modulo-1/aula-16-tech/script.js
@@ -0,0 +1,68 @@
+// var listaConvidados = [
+//     "Jessica",
+//     "Edu",
+//     "Felipe",
+//     "Samantha"
+// ];
+
+// var listaConvidados2 = new Array(
+//     'Fabio',
+//     'Dudu',
+//     'Vic',
+//     'Jansen',
+// );
+// console.log(listaConvidados2)
+
+
+// ARRAY INSERÇÃO
+
+
+// var listaConvid = [];
+
+// listaConvid.push('Amanda');
+// listaConvid.push('Nina', 'Bruno', 'Carol');
+// console.log(listaConvid)
+
+
+// ARRAY REMOÇÃO
+
+// var listaConvid = ['Vê', 'Thomaz', 'Cinthia', 'Luiz', 'Nic'];
+
+// var desconvidados = [listaConvid.pop(), listaConvid.pop()];
+
+// console.log(desconvidados)
+
+
+// Arrays e loops
+
+// var listaConvid = ['Vê', 'Thomaz', 'Cinthia', 'Luiz', 'Nic'];
+// var indice = 0;
+
+// while (indice < listaConvid.length) {
+//     console.log(listaConvid[indice]);
+//     indice++;
+// }
+
+
+// Arrays
+// Arrays são uma estrutura flexível, capaz de armazenar um conjunto de valores
+// em uma única variável.
+// Ex - Sorteios da Loteria:
+
+//sorteio específico
+
+// var sorteio54454 = [12, 32, 45, 60, 42, 7];
+// //histório dos sorteios
+// var sorteios = [
+//     [32, 15, 27, 55, 12, 38],
+//     [25, 17, 13, 22, 06, 01],
+//     [16, 18, 15, 38, 23, 29]
+// ];
+
+
+// Arrays: deleção de elemento
+
+var feira = ['goiaba', 'maçã', 'couve', 'cenoura'];
+feira = feira.splice(2, 2);
+console.log(feira)
+//retorna a parte cortada
