bf33510d5c12b0776520745443ae59bf5447cbd0
Bruno <bpires.siqueira@gmail.com>
2022-10-04 22:46:26 -0300

    arr

--- modulo-1/aula-16-tech/script.js
+++ modulo-1/aula-16-tech/script.js
@@ -62,7 +62,55 @@
 
 // Arrays: deleção de elemento
 
-var feira = ['goiaba', 'maçã', 'couve', 'cenoura'];
-feira = feira.splice(2, 2);
-console.log(feira)
+// var feira = ['goiaba', 'maçã', 'couve', 'cenoura'];
+// feira = feira.splice(2, 2);
+// console.log(feira)
 //retorna a parte cortada
+
+// var nMastigadas = 0;
+// while (nMastigadas < 5) {
+//     console.log(`Mastigada de número ${nMastigadas} realizada`);
+//     nMastigadas++;
+// }
+// console.log('Finalmente engoliu!')
+
+
+// Estruturas de repetição: for
+
+// var feira = ['goiaba', 'maçã', 'couve', 'cenoura'];
+
+// for (var i = 0; i < feira.length; i++) {
+//     console.log(`Fui à feira e comprei: ${feira[i]}`);
+// }
+
+// Laços internos
+// Em alguns cenários precisamos utilizar laços de repetição internos.
+// Exemplo:
+// Temos um array com listas de gastos anotados na semana.
+// Como chegamos a soma de gastos em cada semana?
+
+var gastos = [
+    [32.98, 17.3, 28.4, 55.2],
+    [25.7, 17.13, 13.00]
+]
+
+// for (var i = 0; i < gastos.length; i++) {
+//     var totalSemana = 0;
+//     for (var j = 0; j < gastos[i].length; j++) {
+//         totalSemana += gastos[i][j];
+//     }
+//     console.log(`Gasto na semana ${i}: ${totalSemana}`);
+//     totalSemana = 0;
+// }
+
+function somarArr(arr) {
+    var total = 0;
+    for (var i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+
+for (var i = 0; i < gastos.length; i++) {
+    console.log(`Gasto na semana ${i}: ${somarArr(gastos[i])}`);
+}
