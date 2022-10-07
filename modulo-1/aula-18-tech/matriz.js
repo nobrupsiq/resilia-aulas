97889a43ed43d48ff3d339b0df008a11966d1472
bruno <bpires.siqueira@gmail.com>
2022-10-07 10:51:32 -0300

    exercicio matriz

---
+++ modulo-1/aula-18-tech/matriz.js
@@ -0,0 +1,11 @@
+var matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+
+for (let lin = 0; lin < 3; lin++) {
+    for (let col = 0; col < 3; col++)
+        document.write(matriz[lin][col] + "  ");
+        document.write("<br>");
+}
