20588917204755f599d535aead449d407a56dfb3
bruno <bpires.siqueira@gmail.com>
2022-10-14 08:56:48 -0300

    inicio da aula 1

---
+++ modulo-2/aula-1-tech/querySelector.js
@@ -0,0 +1,5 @@
+function funcao1() {
+   document.querySelector(".exemplo2").style.backgroundColor = "#caf";
+   document.querySelector(".exemplo").style.backgroundColor = "red";
+   document.getElementById("exe").innerHTML = "O meu texto de paragrafo";
+}
