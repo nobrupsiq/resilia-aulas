4ccfa4c72b1da400aef09545cd631909ec89645e
bruno <bpires.siqueira@gmail.com>
2022-10-14 09:38:54 -0300

    elemento 2 ex

---
+++ modulo-2/aula-1-tech/elemento2.js
@@ -0,0 +1,9 @@
+var x = document.querySelectorAll(".exemplo");
+console.log(x);
+
+x[0].innerHTML = "O texto na posição - 0";
+x[1].innerHTML = "O texto na posição - 1";
+x[2].innerHTML = "O texto na posição - 2";
+
+var a = document.title;
+var z = document.URL;
