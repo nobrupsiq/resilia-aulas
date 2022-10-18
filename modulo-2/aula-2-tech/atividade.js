f10c306331d9a662e546c5725074ceb9af0c97df
bruno <bpires.siqueira@gmail.com>
2022-10-18 09:54:38 -0300

    Revert "atividade da aula finalizada"
    This reverts commit 0b89a6381ded7e58e9b32c51ba6cf69cbcd25886.

--- modulo-2/aula-2-tech/atividade.js
+++
@@ -1,9 +0,0 @@
-var btn = document.querySelector("button");
-var ipt = document.querySelector("input");
-
-function activeButton() {
-  const inputValue = ipt.value;
-  alert(`Caractere: (${inputValue}) Código ASCII (${inputValue.charCodeAt()})`);
-}
-
-btn.addEventListener("click", activeButton);
