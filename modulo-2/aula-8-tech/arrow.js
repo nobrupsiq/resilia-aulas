a4bdb1b2e3c91bceb8d60271c4156c1624779e30
bruno <bpires.siqueira@gmail.com>
2022-10-31 09:36:06 -0300

    arrow function ex1

--- modulo-2/aula-8-tech/arrow.js
+++ modulo-2/aula-8-tech/arrow.js
@@ -1,4 +1,12 @@
 function total(a = 10, b = 20) {
     return a + b;
 }
-console.log(total())
+
+// console.log(total())
+
+const addMensagem = (mensagem, data = Date.now()) => {
+    const teste = `${mensagem} ${data}`
+    return teste;
+}
+
+console.log(addMensagem('MILESEGUNDOS'))
