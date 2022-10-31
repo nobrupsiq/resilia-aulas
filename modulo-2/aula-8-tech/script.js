1c4a2e5156de5877d2d129cc919c7dab17af86e2
bruno <bpires.siqueira@gmail.com>
2022-10-31 08:59:46 -0300

    method bind ex 2

--- modulo-2/aula-8-tech/script.js
+++ modulo-2/aula-8-tech/script.js
@@ -18,6 +18,23 @@ let calc = function() {
     return {
         soma: this.soma,
         multiplicar: this.multiplicar,
+        dividir: this.dividir,
     }
 }
-let methods = {}
+
+// Methods enter input
+
+let methods = {
+    soma: function(x, y) {
+        return x + y;
+    },
+    multiplicar: function(x, y) {
+        return x * y;
+    },
+    dividir: function(x, y) {
+        return x / y;
+    }
+}
+
+calculoBind = calc.bind(methods)
+console.log(calculoBind().multiplicar(23, 45))
