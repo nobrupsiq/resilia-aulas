ef222652c2738bbd706bc83737fcc36e7b8a4895
bruno <bpires.siqueira@gmail.com>
2022-11-16 11:04:54 -0300

    instrucoes

--- modulo-3/aula-3-tech/requestXML/xml.js
+++ modulo-3/aula-3-tech/requestXML/xml.js
@@ -16,5 +16,30 @@ xml.onreadystatechange = function () {
   }
 };
 
+// 1 - Criamos um pasta entre na pasta pelo prompt
+// 2 - No prompt de comando do node instalamos:
+// 	npm init
+// 	npm install express
+// 	npm install node-fetch@2
+// 	npm install -g nodemon
+// 3 - Executamos no node com:
+// 	nodemon app.js
+// 	porta 8080 - localhoast:8080
+// 4 - como criar uma pasta no prompt - mkdir "nome da pasta"
+
+// 	Requisição XMLhttpRequest
+// 	ReadyState
+// 	0 - Requisição não iniciada
+// 	1 - Estabelecendo uma requisição com o servidor
+// 	2 - Pedido recebido
+// 	3 - processando o pedido
+// 	4 - Solicitação concluída e resposta pronta
+
+// sites usados:
+// https://expressjs.com/pt-br/starter/installing.html
+// https://www.npmjs.com/package/nodemon
+// api fake
+// https://jsonplaceholder.typicode.com/
+
 xml.open("GET", "https://jsonplaceholder.typicode.com/posts/1");
 xml.send();
