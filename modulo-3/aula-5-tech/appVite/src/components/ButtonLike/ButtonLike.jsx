bb91c98209378c3d4d28af938a4379a5f15a664b
bruno <bpires.siqueira@gmail.com>
2022-11-23 10:26:04 -0300

    aula 5 button finalizada

--- modulo-3/aula-5-tech/appVite/src/components/ButtonLike/ButtonLike.jsx
+++ modulo-3/aula-5-tech/appVite/src/components/ButtonLike/ButtonLike.jsx
@@ -1,3 +1,6 @@
-export default function ButtonLike({ texto }) {
-  return <button class="teste">{texto}</button>;
+import { useState } from "react";
+
+export default function ButtonLike() {
+  const [count, setCount] = useState(0);
+  return <button onClick={() => setCount(count + 1)}>{count}</button>;
 }
