5d071baf276d195892ae1712140b6a6f7f0a346e
bruno <bpires.siqueira@gmail.com>
2022-11-23 09:53:25 -0300

    inicio da aula 5 useStates

---
+++ modulo-3/aula-5-tech/appVite/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ButtonLike from "./components/ButtonLike/ButtonLike.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Teste</h1>
+      <ButtonLike texto="LIKE" />
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo-3/aula-5-tech/appVite/src/components/ButtonLike/ButtonLike.jsx
@@ -0,0 +1,3 @@
+export default function ButtonLike({ texto }) {
+  return <button class="teste">{texto}</button>;
+}
